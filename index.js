ed35e57e31cd6db275fd1004910d78c5c7b985e6
Andrey Mokrousov <insanedeveloper@ya.ru>
2017-12-10 20:50:03 +0300

    parser ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 var fs = require('fs'),
     path = require('path'),
     parser = require('./parser.js'),
     file = fs.readFileSync(path.resolve(process.argv[2]), 'utf-8');
 
-console.log(JSON.strigify(parser(file), null, '  '));
+console.log(JSON.stringify(parser(file), null, '  '));
--- parser.js
+++ parser.js
@@ -1,11 +1,64 @@
 module.exports = function parse(str) {
     var chapters = [];
 
-    str.replace(/=([^=]+)=\n(.+)(!:=(^=))/g, function (match, title, text) {
-        chapters.push({
-            title: title,
-            text: text
-        });
-    })
+    chapters = tokenize(/=([^=]+)=\n+/gm, str);
+
+    chapters = chapters.map(function(chapter) {
+        var commits = tokenize(/==([^=]+)==\n+/gm, chapter.text);
+
+        commits = commits.map(processCommits);
+
+        return {
+            title: chapter.token,
+            commits: commits
+        };
+
+    });
+
     return chapters;
 };
+
+function processCommits(commit, i, commits) {
+    console.log('commit', commit);
+    var match = commit.text.match(/(?:<-([^\n]+)\n)?(\d\d:\d\d\s\d\d\.\d\d\.\d\d)\n+([\s\S]+)/m);
+
+    return {
+        title: commit.token,
+        parent: match[1],
+        time: match[2],
+        text: match[3]
+    }
+};
+
+function tokenize(re, str) {
+    var matches = [],
+        res = [];
+
+
+    str.replace(re, function (match, token, index) {
+        console.log(token, index, index + match.length);
+        matches.push({
+            content: token,
+            start: index,
+            end: index + match.length
+        });
+    });
+
+
+    for (var i = 0; i < matches.length - 1; i++) {
+        var from = matches[i].end,
+            to = matches[i + 1].start;
+
+        res.push({
+            token: matches[i].content,
+            text: str.slice(from, to)
+        });
+    }
+    var last = matches.pop();
+    res.push({
+        token: last.content,
+        text: str.slice(last.end, str.length)
+    });
+    return res;
+
+}
